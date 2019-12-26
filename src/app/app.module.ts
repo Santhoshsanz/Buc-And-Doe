9f9e591e29c7fd1a88c637d529d45dd590fa961b
Santhosh Kumar <santhoshkumar@b-one.com>
2019-12-26 14:40:28 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './private/home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopBarComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
